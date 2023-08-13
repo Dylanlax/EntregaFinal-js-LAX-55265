8c44f5ae530f9ecc7b5785c6f80c990f3ba14128
dylanlax <dylan.lax321@gmail.com>
2023-08-13 16:02:27 -0300

    intento2 me funciono el buscador

--- js/main.js
+++ js/main.js
@@ -8,10 +8,21 @@ function bienvenida(){
 
 bienvenida()
 
-document.addEventListener("keyup", e => { //aca le pido que cuando suelto la tecla haga la E
+/* document.addEventListener("keyup", e => { //aca le pido que cuando suelto la tecla haga la E
     if (e.target.matches("#buscador")) {
         document.querySelectorAll(".articulo").forEach(componente => { // aca le pido que recorra todos los articulos
             componente.textContent.toLowerCase().includes(e.target.value) // devuelve true o false si hay coincidencia
+            ? componente.classList.remove("filtro")
+            : componente.classList.add("filtro") // Use operador de IF y ELSE, le pido que si no coincide agregue la clase filtro y esconda la busqueda
+        })
+    }
+}) */ // ESTE FUE MI PRIMER INTENTO
+
+document.addEventListener("keyup", e => { //aca le pido que cuando suelto la tecla haga la E
+    if (e.target.matches("#buscador")) {
+        document.querySelectorAll(".articulo").forEach(componente => { // aca le pido que recorra todas las cards
+            const parrafo = componente.querySelector("p:first-of-type"); // Creo una variable que solo lea los primeros parrafos de cada card, porque el segundo parrafo es el precio
+            const match = parrafo.textContent.toLowerCase().includes(e.target.value)
             ? componente.classList.remove("filtro")
             : componente.classList.add("filtro") // Use operador de IF y ELSE, le pido que si no coincide agregue la clase filtro y esconda la busqueda
         })
