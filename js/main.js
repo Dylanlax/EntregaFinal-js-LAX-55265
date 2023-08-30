a72037337fd6c39316c65ecc1d425e933a2a771d
dylanlax <dylan.lax321@gmail.com>
2023-08-30 01:43:55 -0300

    rehaciendo todo

--- js/main.js
+++ js/main.js
@@ -1,195 +1,118 @@
 
-const saveLocal = () => {
-    localStorage.setItem("carrito", JSON.stringify(allProducts));
-};
+// Cartel bienvenida
 
-// Variable de carrito
-let allProducts = []
 
-const loadLocal = () => {
-    const storedCart = localStorage.getItem("carrito");
-    if (storedCart) {
-        allProducts = JSON.parse(storedCart);
-        
-    }
-};
-
-loadLocal()
-
-document.addEventListener("keyup", e => { //aca le pido que cuando suelto la tecla haga la E
-    if (e.target.matches("#buscador")) {
-        document.querySelectorAll(".articulo").forEach(componente => { // aca le pido que recorra todas las cards
-            const parrafo = componente.querySelector("p:first-of-type"); // Creo una variable que solo lea los primeros parrafos de cada card, porque el segundo parrafo es el precio
-            const match = parrafo.textContent.toLowerCase().includes(e.target.value)
-            ? componente.classList.remove("filtro")
-            : componente.classList.add("filtro") // Use operador de IF y ELSE, le pido que si no coincide agregue la clase filtro y esconda la busqueda
-        })
-    }
+/* const cartelito = () => {
+    (async() => {
+        const { value: email } = await Swal.fire({
+    title: 'Ingrese su mail para recibir las ultimas novedades',
+    input: 'email',
+    inputLabel: 'Ingrese su mail',
+    inputPlaceholder: 'Enter your email address'
 })
 
-
-
-/* -------------------------------------------------*/
-
-// INTENTO DE CARRITO
-
-
-// Lista de contenedores de productos
-const listaDeProductos = document.querySelector(".productList")
-const rowProduct = document.querySelector(".row-product")
-
-
-
-
-const valorTotal = document.querySelector('.total-pagar')
-
-const countProducts = document.querySelector('#contador-productos')
-
-
-listaDeProductos.addEventListener("click", i => {
-    if(i.target.classList.contains("btn-add-cart")){
-        const product = i.target.parentElement // LE PIDO QUE SI DOY CLICK EN BUTTON SELECCIONE EL PADRE DE LA CARD QUE CONTIENE ESE BUTTON
-
-
-        
-        
-        const infoProduct = {
-            quantity: 1,
-            title: product.querySelector("p:first-of-type").textContent, // Le pido que lea el nombre del producto
-            price: product.querySelector("p:nth-of-type(2n)").textContent, // le pido que lea el precio del producto
-
-        }
-
-        const exits = allProducts.some(product => product.title === infoProduct.title)
-
-        if (exits) {
-            const products = allProducts.map(product => {
-                if(product.title === infoProduct.title){
-                    product.quantity++
-                    return product
-                } else {
-                    return product // ESTO ES PARA SUMAR EL CONTADOR DE QUANTITY
-                }
-            })
-            allProducts = [...products]
-        } else {
-            allProducts = [...allProducts, infoProduct]
-        }
-
-        
-        saveLocal()
-        showHTML()
-        
-    }
-})
-
-
-
-// Funcion para localstorage
-
-
-//set item
-
-
-
-
-
-
-
-// funcion de carrito
-
-const btnCart = document.querySelector('.container-icon')
-const containerCartProducts = document.querySelector('.container-cart-products')
-
-btnCart.addEventListener('click', () => {
-    containerCartProducts.classList.toggle('hidden-cart')
-})
-
-
-// para eliminar del carrito
-
-rowProduct.addEventListener('click', (e) => {
-    if(e.target.classList.contains('icon-close')){
-        const product = e.target.parentElement
-        const title = product.querySelector('p').textContent
-
-
-        allProducts = allProducts.filter( product => product.title !== title);
-
-    saveLocal()
-        showHTML()
-        
-    }   
-})
-
-
-
-
-const showHTML = () => {
-
-
-    
-
-    // Limpiar html
-
-    rowProduct.innerHTML= '';
-
-
-    // total del carrito
-
-    let total = 0;
-    let totalOfProducts = 0;
-
-
-
-    allProducts.forEach(product => {
-        const containerProduct = document.createElement("div")
-        containerProduct.classList.add("cart-product")
-
-        containerProduct.innerHTML=`
-        
-
-    <div class="info-cart-product">
-                <span class="cantidad-producto-carrito">${product.quantity}</span>
-                <p class="titulo-producto-carrito">${product.title}</p>
-                <span class="precio-producto-carrito">${product.price}</span>
-            </div>
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="icon-close"
-            >
-                <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M6 18L18 6M6 6l12 12"
-                />
-            </svg>
-        
-        `
-
-        rowProduct.append(containerProduct);
-
-        
-
-        // para actualizar el valor
-
-        total = total + parseInt(product.quantity * product.price.slice(1));
-    
-        totalOfProducts = totalOfProducts + product.quantity;
-        
-    });
-
-valorTotal.innerText = `$${total}`;
-
-countProducts.innerText = totalOfProducts;
-
-saveLocal();
-
-
+if (email) {
+    Swal.fire(`Entered email: ${email}`)
+}
+    })()
 }
 
-showHTML()
+setTimeout(cartelito,500) */
+
+
+const shopContent = document.getElementById("shopContent")
+const verCarrito = document.getElementById("verCarrito")
+const modalContainer = document.getElementById("modal-container")
+
+
+const productos = [{ nombre: "PROCESADOR INTEL I5 11400", precio: 100000, imagen: "img/procesador intel i5.jpg", id:1},
+    {nombre: "PROCESADOR AMD RYZEN 5 4500", precio: 100000, imagen: "img/ryzen 5 4500.jpg", id: 2},
+    {nombre: "Grafica rtx3060ti", precio: 250000, imagen: "img/3060ti.png", id: 3},
+    {nombre: "PROCESADOR INTEL CELERON", precio: 50000, imagen: "img/intel celeron.jpg", id: 4},
+    {nombre: "GRAFICA RX 580", precio: 100000, imagen: "img/rx580.jpg", id: 5}
+]
+
+
+// Variable de carrito
+let carrito = []
+
+//INYECCION DE PRODUCTOS
+
+productos.forEach((product)=> {
+    let content = document.createElement("div");
+    content.className = "card1";
+    content.innerHTML = `
+        <img src="${product.imagen}">
+        <h3>${product.nombre}</h3>
+        <p class="price">$ ${product.precio}</p>
+    `;
+
+
+    shopContent.append(content);
+
+    let comprar = document.createElement("button");
+    comprar.innerText = "comprar";
+
+    content.append(comprar);
+
+
+    comprar.addEventListener("click", () =>{
+        carrito.push({
+            id: product.id,
+            imagen: product.imagen,
+            nombre: product.nombre,
+            precio: product.precio,
+        })
+        console.log(carrito)
+    })
+});
+
+
+// FUNCION DE CARRITO
+
+verCarrito.addEventListener("click", () => {
+
+    modalContainer.innerHTML = ``;
+
+    modalContainer.style.display ="flex";
+
+    const modalHeader = document.createElement("div");
+    modalHeader.className = "modal-header"
+    modalHeader.innerHTML = `
+        <h2 class="modal-header-title">Carrito</h2>
+    `;
+modalContainer.append(modalHeader);
+
+const modalButton = document.createElement("h4");
+modalButton.innerText = "x";
+modalButton.className = "modal-header-button";
+
+modalButton.addEventListener("click", () => {
+    modalContainer.style.display = "none";
+})
+
+
+modalHeader.append(modalButton);
+
+carrito.forEach((product) => {
+    let carritoContent = document.createElement("div")
+    carritoContent.className = "modal-content"
+    carritoContent.innerHTML = `
+    <img src="${product.imagen}">
+    <h3>${product.nombre}</h3>
+    <p>$ ${product.precio}</p>
+    `;
+
+    modalContainer.append(carritoContent)
+});
+
+    // TOTAL DEL CARRITO
+
+    const total = carrito.reduce((acc, el) => acc + el.precio, 0)
+
+    const totalBuying = document.createElement("div")
+    totalBuying.className = "total-content"
+    totalBuying.innerHTML = `Total a pagar: $ ${total}`;
+    modalContainer.append(totalBuying)
+});
+
